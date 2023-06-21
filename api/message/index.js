d74683b94bc774454785ca53581a4aa413b78ac4
David Jennings <david@davidjennings.co.uk>
2023-06-21 22:17:23 +0100

    secret

--- api/message/index.js
+++ api/message/index.js
@@ -2,6 +2,6 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res.json({
-        purpleSock: "Hello from the Azure Functions API"
+        purpleSock: `Hello from the Azure Functions API - secret:${process.env.SECRET}` 
     });
 }
